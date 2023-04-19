511e6f09a246db4bbd8970ff9fe764905636ea7d
Sourcery AI <>
2023-04-19 04:46:09 +0000

    'Refactored by Sourcery'

--- backend/technologies/validators.py
+++ backend/technologies/validators.py
@@ -5,5 +5,5 @@ from django.core.exceptions import ValidationError
 def validate_file_extension(value):
   ext = os.path.splitext(value.name)[1]  # [0] returns path and filename
   valid_extensions = ['.svg'] # populate with the allowed extensions
-  if not ext.lower() in valid_extensions:
+  if ext.lower() not in valid_extensions:
     raise ValidationError('Unsupported file extension.')
